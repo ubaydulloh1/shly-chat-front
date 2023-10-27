24577cdc7939744bb21e2127b2617bd72a7595d8
Ubaydulloh <ubaydulloh1000@gmail.com>
2023-10-27 12:23:04 +0200

    deploy script added

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,13 @@
+#!/usr/bin/bash
+
+# configure nvm & node
+source ~/.nvm/nvm.sh
+echo "nvm version is: $(nvm --version)"
+nvm use 18
+echo "node version is: $(node --version)"
+
+# npm install && build
+npm install && npm run build
+
 # reload nginx
-sudo nginx -s reload
+sudo ./d.sh
